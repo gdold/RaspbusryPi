da944c5321e86bb27a1ae0d80388296bcfd2d18f
Gavin Dold <gdold42@gmail.com>
2016-05-12 22:51:43 +0100

    Neatened code

--- RaspbusryPi.py
+++ RaspbusryPi.py
@@ -11,18 +11,12 @@ import BusDisplay as dsp
 import time
 import threading
 import sys
-import signal
 import requests
 
-
-def strip_spaces(string):
-    return string.replace(', ',',')
-
-update = True
     
 stop_id = '490008357S'
 api_key = '?app_id=f1fff9f7&app_key=80c7973c8a779d54159f53c71bdfa5e6'
-bus = "W7"
+description = "W7 to F'buryPark"
 fetch_delay = 30
 refresh_delay = 5
 
@@ -31,15 +25,15 @@ backlight_g = 33
 backlight_b = 33
 lcd_contrast = 43
     
-Stop = fbd.FetchBusData(stop_id,api_key)
-Disp = dsp.Displayotron()
+def strip_spaces(string):
+    return string.replace(', ',',')
 
 def fetch_bus_data():
     Stop.fetch_bus_data()
 
 def display_bus_times():
     lcd.clear()
-    Disp.write_line(0,bus + " from " + stop_id)
+    Disp.write_line(0,description)
     if len(Stop.busstr) > 16:
         Disp.write_line(1,strip_spaces(Stop.busstr))
     else:
@@ -50,16 +44,19 @@ def refresh_display():
     Stop.refresh_bus_times()
     display_bus_times()
 
+Stop = fbd.FetchBusData(stop_id,api_key)
+Disp = dsp.Displayotron()
 
 Disp.tidyup()
 
 Disp.set_rgb(backlight_r,backlight_g,backlight_b)
 lcd.set_contrast(lcd_contrast)
 
+# Make sure there is data right from start
 fetch_bus_data()
 refresh_display()
 
-
+# Two asynchronous threads that update and refresh display at set intervals
 FetchUpdater = upd.Updater(fetch_delay,fetch_bus_data)
 RefreshUpdater = upd.Updater(refresh_delay,refresh_display)
 FetchUpdater.start_updating()
@@ -78,21 +75,3 @@ def handle_cancel(ch, evt):
     
     Disp.tidyup()
     sys.exit(0)
-
-
-#Updater.stop_updating()
-
-# if __name__ == '__main__':
-    # try:
-        # try: input = raw_input # Compatibility with Python 2 & 3
-        # except NameError: pass
-        # input("") # Main thread ends on user pressing enter
-        # Updater.stop_updating()
-        # Disp.tidyup()
-    # except KeyboardInterrupt:
-        # print('Keyboard interrupt')
-        # Updater.stop_updating()
-        # Disp.tidyup()
-        # sys.exit(0)
-
-#signal.pause() # Alternate option
